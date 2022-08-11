698482669c5181d520af66ec692d5f2ce4986738
Dolorrr Lapule <103165024+yagirldolor@users.noreply.github.com>
2022-08-11 18:11:49 +0000

    updates file routing

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs')
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  var title = 'Our Home Page';
+  var heading = 'My Website Name';
+  res.render('pages/index',{
+    'title':title,
+    'heading':heading
+  });//key:data
+})
+
+app.get('/about', (req, res) => {
+  var title = 'Our About Page';
+  var heading = 'My Website Name';
+  res.render('pages/about',{
+    'title':title,
+    'heading':heading
+  });//key:data
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
