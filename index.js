9abb0de5b97ecd30be11a3af30aad1d177bd2ab1
Dolorrr Lapule <103165024+yagirldolor@users.noreply.github.com>
2022-08-22 01:04:43 +0000

    Adds view users

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+var data = require('./data/test.json');
 app.set('view engine','ejs')
 //this will allow us to serve up static files, CSS, images & JS
 app.use(express.static(__dirname));
@@ -42,6 +42,39 @@ app.get('/culture', (req, res) => {
   });//key:data
 })
 
+app.get('/contact', (req, res) => {
+  var title = `Let's Connect!`;
+  var heading = 'My Culture Page';
+  res.render('pages/culture',{
+    'title':title,
+    'heading':heading
+  });//key:data
+})
+
+app.get('/users', (req, res) => {
+  var title = 'My Users Page';
+  var heading = 'My Culture Page';
+  res.render('users/index',{
+    'title':title,
+    'heading':heading,
+    'users':data
+  });//key:data
+})
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+  var heading = "My Users Page"
+ res.render('users/view', {
+     title: title,
+     'heading':heading,
+     user: data[--id]
+ });
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(data);
 })
